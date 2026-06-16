6f70b3a782933a29070e40554a0ff5e057b8c74b
AkshayKharat <134252944+Akshay6661@users.noreply.github.com>
2026-06-16 17:37:57 +0530

    Update debug.py

--- debug.py
+++ debug.py
@@ -1,18 +1,22 @@
-Avg KPI Value % = 
-IF(
-    SELECTEDVALUE(fact_quality[KPI Type]) = "Percentage",
-    AVERAGEX(
-        VALUES(fact_quality[Date_1]),
-        MAX(fact_quality[Value])
-    )
+Avg KPI Value Smart = 
+SWITCH(
+    SELECTEDVALUE(fact_quality[KPI Type]),
+    "Percentage", AVERAGEX(VALUES(fact_quality[Date_1]), MAX(fact_quality[Value])),
+    "Binary", AVERAGEX(VALUES(fact_quality[Date_1]), MAX(fact_quality[Value])),
+    "Count", SUMX(VALUES(fact_quality[Date_1]), MAX(fact_quality[Value]))
 )
 
 
-Avg KPI Target % = 
-IF(
-    SELECTEDVALUE(fact_quality[KPI Type]) = "Percentage",
-    AVERAGEX(
-        VALUES(fact_quality[Date_1]),
-        MAX(dim_client_kpi_config[Target])
-    )
+Avg KPI Target Smart = 
+SWITCH(
+    SELECTEDVALUE(fact_quality[KPI Type]),
+    "Percentage", AVERAGEX(
+                    VALUES(fact_quality[Date_1]), 
+                    MAX(dim_client_kpi_config[Target])
+                 ),
+    "Binary",     AVERAGEX(
+                    VALUES(fact_quality[Date_1]), 
+                    MAX(dim_client_kpi_config[Target])
+                 ),
+    "Count",      MAX(dim_client_kpi_config[Target])
 )
