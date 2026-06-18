fb41884b5abe095788195a473752c17dc1d2a6fa
AkshayKharat <134252944+Akshay6661@users.noreply.github.com>
2026-06-18 16:44:11 +0530

    Rename Avg KPI Target to Avg KPI Value

--- debug.py
+++ debug.py
@@ -1,18 +1,18 @@
-Avg KPI Target Smart = 
+Avg KPI Value Smart = 
 VAR KPIType = SELECTEDVALUE(fact_quality[KPI Type])
 RETURN
 SWITCH(
     KPIType,
     "Percentage", AVERAGEX(
                     VALUES(fact_quality[Date_1]),
-                    [KPI Target Trend %]
+                    [KPI Value Trend %]
                   ),
     "Binary",     AVERAGEX(
                     VALUES(fact_quality[Date_1]),
-                    [KPI Target Trend Binary]
+                    [KPI Value Trend Binary]
                   ),
     "Count",      AVERAGEX(
                     VALUES(fact_quality[Date_1]),
-                    [KPI Target Trend Count]
+                    [KPI Value Trend Count]
                   )
 )
