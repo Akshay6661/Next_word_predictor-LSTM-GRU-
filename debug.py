2f118db45e5cbf8297af4bdd18ec28c9b182b8e4
AkshayKharat <134252944+Akshay6661@users.noreply.github.com>
2026-06-18 16:41:12 +0530

    Update debug.py

--- debug.py
+++ debug.py
@@ -11,25 +11,8 @@ SWITCH(
                     VALUES(fact_quality[Date_1]),
                     [KPI Target Trend Binary]
                   ),
-    "Count",      MAX([KPI Target Trend Count])
-)
-
-
-Avg KPI Value Smart = 
-VAR KPIType = SELECTEDVALUE(fact_quality[KPI Type])
-RETURN
-SWITCH(
-    KPIType,
-    "Percentage", AVERAGEX(
+    "Count",      AVERAGEX(
                     VALUES(fact_quality[Date_1]),
-                    [KPI Value Trend %]
-                  ),
-    "Binary",     AVERAGEX(
-                    VALUES(fact_quality[Date_1]),
-                    [KPI Value Trend Binary]
-                  ),
-    "Count",      SUMX(
-                    VALUES(fact_quality[Date_1]),
-                    [KPI Value Trend Count]
+                    [KPI Target Trend Count]
                   )
 )
