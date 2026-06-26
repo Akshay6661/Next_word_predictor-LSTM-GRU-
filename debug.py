1f3e67657ff6917e7010914d1f54b7c5e0d6d0e3
AkshayKharat <134252944+Akshay6661@users.noreply.github.com>
2026-06-26 13:10:19 +0530

    Update debug.py

--- debug.py
+++ debug.py
@@ -1,17 +1,17 @@
-Client Group = 
+Client Brand = 
 SWITCH(
     TRUE(),
     Dim_Quality_Client[Client_Name] IN {
-        "Pfizer GO DSU","Pfizer PSSR"
-    }, Dim_Quality_Client[Service_Type],
+        "Pfizer GO DSU", "Pfizer PSSR"
+    }, "Pfizer",
     Dim_Quality_Client[Client_Name] IN {
-        "Vantive AR","Vantive CP"
+        "Vantive AR", "Vantive CP"
     }, "Vantive",
     Dim_Quality_Client[Client_Name] IN {
-        "GE-HC AR","GE-HC ICSR"
+        "GE-HC AR", "GE-HC ICSR"
     }, "GE",
     Dim_Quality_Client[Client_Name] IN {
-        "MICC-Glenmark","MICC-Piramal","MICC-Cipla"
+        "MICC-Glenmark", "MICC-Piramal", "MICC-Cipla"
     }, "MICC",
     Dim_Quality_Client[Client_Name]
 )
