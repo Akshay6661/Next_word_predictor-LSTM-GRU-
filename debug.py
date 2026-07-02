b4098a3339fd11760c85266f68d553c1cb7e0c60
AkshayKharat <134252944+Akshay6661@users.noreply.github.com>
2026-07-02 11:52:29 +0530

    Update debug.py

--- debug.py
+++ debug.py
@@ -1,56 +1,22 @@
-KPI Met Flag = 
-VAR ActualVal = MAX(fact_quality[Value])
-VAR TargetVal = MAX(dim_client_kpi_config[Target])
-VAR Direction = SELECTEDVALUE(dim_kpi_master[Direction])
-VAR KPIType = SELECTEDVALUE(fact_quality[KPI Type])
 
-RETURN
-SWITCH(
-    TRUE(),
-    KPIType = "Binary",
-        IF(ActualVal >= TargetVal, 1, 0),
-    KPIType = "Percentage" && Direction = "Higher Better",
-        IF(ActualVal >= TargetVal, 1, 0),
-    KPIType = "Percentage" && Direction = "Lower Better",
-        IF(ActualVal <= TargetVal, 1, 0),
-    KPIType = "Count" && Direction = "Lower Better",
-        IF(ActualVal <= TargetVal, 1, 0),
-    KPIType = "Count" && Direction = "Higher Better",
-        IF(ActualVal >= TargetVal, 1, 0)
-)
-
-
-
-Met Count = 
-CALCULATE(
-    COUNTROWS(fact_quality),
-    [KPI Met Flag] = 1
-)
-
-
-Not Met Count = 
-CALCULATE(
-    COUNTROWS(fact_quality),
-    [KPI Met Flag] = 0
-)
-
-
-Overall Compliance % = 
-DIVIDE(
-    [Met Count],
-    [Met Count] + [Not Met Count],
-    0
-)
-
-
-Client Avg Performance % = 
-CALCULATE(
-    AVERAGEX(
-        VALUES(fact_quality[KPI_ID]),
-        AVERAGEX(
-            VALUES(fact_quality[Date_1]),
-            MAX(fact_quality[Value])
+Forecast Smart = 
+SUMX(
+    VALUES(Dim_SOW[SOW_Key]),
+    VAR Frequency = MAX(Dim_SOW[Sow_Frequency])
+    VAR YearlyForecast = MAX(Dim_SOW[Volume_Forecast])
+    VAR MonthlyForecast = CALCULATE(SUM(Fact_Forecast[Volume_Forecast]))
+    RETURN
+    IF(
+        ISBLANK(YearlyForecast) && ISBLANK(MonthlyForecast),
+        BLANK(),
+        IF(
+            Frequency = "Yearly",
+            IF(
+                ISINSCOPE(Dim_SOW[SOW_Name]),
+                MonthlyForecast,
+                YearlyForecast
+            ),
+            MonthlyForecast
         )
-    ),
-    fact_quality[KPI Type] = "Percentage"
+    )
 )
