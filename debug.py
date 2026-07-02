649439e02b86bfcb1c94994096dfda58709b9428
AkshayKharat <134252944+Akshay6661@users.noreply.github.com>
2026-07-02 12:24:27 +0530

    Update debug.py

--- debug.py
+++ debug.py
@@ -1,12 +1,12 @@
-Forecast Smart = 
+Forecast Smart =
 SUMX(
     VALUES(Dim_SOW[SOW_Key]),
     VAR Frequency = MAX(Dim_SOW[Sow_Frequency])
-    VAR YearlyForecast = SUM(Dim_SOW[Volume_Forecast])
+    VAR YearlyForecast = MAX(Dim_SOW[Volume_Forecast])
     VAR MonthlyForecast = CALCULATE(SUM(Fact_Forecast[Volume_Forecast]))
     RETURN
     IF(
-        ISBLANK(YearlyForecast) && ISBLANK(MonthlyForecast),
+        ISBLANK(Frequency) && ISBLANK(YearlyForecast),
         BLANK(),
         IF(
             Frequency = "Yearly",
