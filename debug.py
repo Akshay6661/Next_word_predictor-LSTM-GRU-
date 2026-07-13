546bd9f58b62ab968ac47540c73caf523559a057
AkshayKharat <134252944+Akshay6661@users.noreply.github.com>
2026-07-13 15:10:29 +0530

    Update debug.py

--- debug.py
+++ debug.py
@@ -1,9 +1,16 @@
 Achievement Status Calc = 
 VAR _kpiid = fact_quality[KPI_ID]
-VAR _type = LOOKUPVALUE(dim_kpi_master[KPI Type], dim_kpi_master[KPI_ID], _kpiid)
-VAR _direction = LOOKUPVALUE(dim_kpi_master[Direction], dim_kpi_master[KPI_ID], _kpiid)
+VAR _clientkey = fact_quality[Client_Key]
+VAR _type = fact_quality[KPI Type]
 VAR _value = fact_quality[Value]
-VAR _target = LOOKUPVALUE(dim_client_kpi_config[Target], dim_client_kpi_config[KPI_ID], _kpiid)
+VAR _direction = LOOKUPVALUE(dim_kpi_master[Direction], dim_kpi_master[KPI_ID], _kpiid)
+VAR _target = LOOKUPVALUE(
+                dim_client_kpi_config[Target], 
+                dim_client_kpi_config[KPI_ID], _kpiid,
+                dim_client_kpi_config[Client_Key], _clientkey
+              )
+VAR _defaultTarget = LOOKUPVALUE(dim_kpi_master[Default Target], dim_kpi_master[KPI_ID], _kpiid)
+VAR _effectiveTarget = COALESCE(_target, _defaultTarget)
 RETURN
 SWITCH(
     TRUE(),
@@ -11,13 +18,13 @@ SWITCH(
         SWITCH(TRUE(), _value = 1, "Achieved", _value = 0, "Not Achieved", "Invalid Binary Value"),
     _type = "Count",
         SWITCH(TRUE(),
-            _direction = "Higher Better", IF(_value >= _target, "Achieved", "Not Achieved"),
-            _direction = "Lower Better", IF(_value <= _target, "Achieved", "Not Achieved"),
+            _direction = "Higher Better", IF(_value >= _effectiveTarget, "Achieved", "Not Achieved"),
+            _direction = "Lower Better", IF(_value <= _effectiveTarget, "Achieved", "Not Achieved"),
             "No Direction Set"),
     _type = "Percentage",
         SWITCH(TRUE(),
-            _direction = "Higher Better", IF(ROUND(_value,4) >= ROUND(_target,4), "Achieved", "Not Achieved"),
-            _direction = "Lower Better", IF(ROUND(_value,4) <= ROUND(_target,4), "Achieved", "Not Achieved"),
+            _direction = "Higher Better", IF(ROUND(_value,4) >= ROUND(_effectiveTarget,4), "Achieved", "Not Achieved"),
+            _direction = "Lower Better", IF(ROUND(_value,4) <= ROUND(_effectiveTarget,4), "Achieved", "Not Achieved"),
             "No Direction Set"),
     "Type Not Set"
 )
